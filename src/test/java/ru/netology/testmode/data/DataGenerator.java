62587761126acbdcea2949f8a353cae2b6417a44
ElenaIlinykh <bengaliki22@gmail.com>
2022-10-26 12:45:01 +0700

    Project changed

--- src/test/java/ru/netology/testmode/data/DataGenerator.java
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -6,11 +6,11 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import lombok.Value;
-import lombok.val;
 
 import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
+
 
 public class DataGenerator {
     private static final RequestSpecification requestSpec = new RequestSpecBuilder()
